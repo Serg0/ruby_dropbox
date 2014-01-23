57392dd50167b2f053d55d7373126180f52af979
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2014-01-23 15:40:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dropbox::Application.config.secret_key_base = '72791456d5fee461e399e765516f922d55faa4458317118116bf7a832d8eb1d96fe3de855c508a200c9c238032e186acc5fd128d7d971f13d41a6947a6c8929f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dropbox::Application.config.session_store :cookie_store, key: '_dropbox_session'
