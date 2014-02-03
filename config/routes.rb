7fd93a397f9758ada1380167c7b38f19d57a1fe1
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2014-02-03 19:02:31 +0200

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Dropbox::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+    root 'users#index'
+
+  resources :users
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
