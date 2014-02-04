dd492ccae818ee2bb9ce9d1585b2f671438a5fd2
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2014-02-04 16:45:28 +0200

    added devise sign in\sign out, styles in pro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
 
   # GET /users
   # GET /users.json
-  before_action :authenticate_user!
 
+  before_action :authenticate_user!
   def index
     @users = User.all
+
   end
 
   # GET /users/1
