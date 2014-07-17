ad214c6b3586992b17d751da1c5dd0a8bf63c518
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2014-07-17 18:41:11 +0300

    add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716131913) do
+ActiveRecord::Schema.define(version: 20140717144127) do
 
   create_table "friendships", force: true do |t|
     t.integer "friendable_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140716131913) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
